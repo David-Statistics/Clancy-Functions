fc5cfe55fa072ed70ea3db0e817937d53ad3c8b6
David Clancy <Clancy@Davids-MacBook-Pro.local>
2016-10-11 17:12:30 -0600

    Add new function panel.cor

---
+++ R/panel.cor.r
@@ -0,0 +1,26 @@
+#' Used to replace duplicate scatterplots in a matrix
+#'
+#' @export
+#' @example pairs(iris, upper.panel = panel.cor)
+#'
+#'
+#'
+panel.cor <- function(x, y, digits = 2, cex.cor, include.p = FALSE, ...)
+{
+  usr <- par("usr"); on.exit(par(usr))
+  par(usr = c(0, 1, 0, 1))
+  # correlation coefficient
+  r <- cor(x, y)
+  txt <- format(c(r, 0.123456789), digits = digits)[1]
+  txt <- paste("r= ", txt, sep = "")
+  text(0.5, 0.6, txt)
+
+  if(include.p){
+    # p-value calculation
+    p <- cor.test(x, y)$p.value
+    txt2 <- format(c(p, 0.123456789), digits = digits)[1]
+    txt2 <- paste("p= ", txt2, sep = "")
+    if(p<0.01) txt2 <- paste("p= ", "<0.01", sep = "")
+    text(0.5, 0.4, txt2)
+  }
+}
